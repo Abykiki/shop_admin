3acfbdb4b3724938b1c1dc74d3a0e6005689cc24
Abykiki <ww.Sun@qq.com>
2018-12-21 22:18:00 +0800

    Home组件完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,143 @@
 <template>
-  <div>组件</div>
+  <el-container class="home">
+    <el-header>
+      <div class="logo"></div>
+      <div class="title">
+        <h1>电商管理系统</h1>
+      </div>
+      <div class="logout">
+        <span>欢迎光临</span>
+        <a href="javascript:;" @click="logout">退出</a>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <!--
+          el-menu: 整个导航菜单组件
+          default-active: 默认激活的菜单\
+          background-color: 导航菜单背景色
+          active-text-color： 激活的文字的颜色
+
+          el-submenu: 子菜单
+          template: 制订了子菜单的文本和图标
+          el-menu-item-group: 菜单项的分组
+          el-menu-item: 菜单项
+        -->
+        <el-menu
+          default-active="1-1"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <!-- 菜单项 -->
+            <el-menu-item index="/users">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <!-- 菜单项 -->
+            <el-menu-item index="2-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {}
+// 退出功能
+// 1.点击退出按钮
+// 2.弹出一个确认框
+// 3.点击确定,删除token,跳转到登录组件
+export default {
+  methods: {
+    logout() {
+      this.$confirm('你确定要退出系统吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '退出成功!'
+          })
+          // 删除token
+          localStorage.removeItem('token')
+          this.$router.push('/login')
+        })
+        .catch(() => {
+          this.$message.info('取消退出')
+        })
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+// lang设置语言 less sass stylus
+// scoped: 如果设置了,表示样式只会在当前组件生效
+.home {
+  height: 100%;
+  .el-header {
+    display: flex;
+    background-color: #b3c1cd;
+    .logo {
+      width: 180px;
+      background-image: url('~@/assets/logo.png');
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: center center;
+    }
+    .logout {
+      width: 180px;
+      text-align: right;
+      line-height: 60px;
+      font-weight: 700;
+      a {
+        color: orange;
+      }
+    }
+    .title {
+      flex: 1;
+      h1 {
+        text-align: center;
+        line-height: 60px;
+        color: #fff;
+        font-size: 30px;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-main {
+    background-color: #eaeef1;
+  }
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -38,4 +38,5 @@ router.beforeEach((to, from, next) => {
     next('/login')
   }
 })
+
 export default router
